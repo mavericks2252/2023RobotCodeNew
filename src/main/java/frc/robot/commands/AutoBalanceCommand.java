0267945f343847b4b33f73b98ae01a46f0eb378b
mavericks2252 <dlangdon@ehove.net>
2023-02-15 20:07:17 -0500

    Auto Balance Command & Auto Path Class
    Added AutoBalance Command and Auto Path creation Class

--- src/main/java/frc/robot/commands/AutoBalanceCommand.java
+++ src/main/java/frc/robot/commands/AutoBalanceCommand.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
